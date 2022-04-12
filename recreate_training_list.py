eea416a44a1982341f3bd42113c2c0b5a4b773ef
Chang-Paris <qi.chang1022@gmail.com>
2022-04-12 18:08:34 +0200

    Recreate depths.txt file
    Signed-off-by: Chang-Paris <qi.chang1022@gmail.com>

--- recreate_training_list.py
+++ recreate_training_list.py
@@ -19,8 +19,15 @@ with open(os.path.join(original_file_folder, "imageset_test.txt"), 'r') as f:
 
 
 root_folder = "data"
+depths = []
 for chunk in os.listdir(data_folder):
-    print(chunk)
+    current_chunk = os.path.join(data_folder, chunk)
+    depth_folder = os.path.join(current_chunk, "depths")
+    current_depths = [os.path.join(root_folder, os.path.join(chunk, os.path.join("depths", filename))) for filename in sorted(os.listdir(depth_folder))]
+    depths.extend(current_depths)
+
+with open(os.path.join(output_folder,"depth_list.txt"), "w") as outfile:
+    outfile.write("\n".join(depths))
 # todo depth_list.txt
 # todo image_list.txt
 # todo imageset_all.txt
