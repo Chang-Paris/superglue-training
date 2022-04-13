f8b8e8403e8140cee085f8ee14a1a5218b8efdaf
Chang-Paris <qi.chang1022@gmail.com>
2022-04-13 11:36:00 +0200

    Get undist images list
    Signed-off-by: Chang-Paris <qi.chang1022@gmail.com>

--- recreate_training_list.py
+++ recreate_training_list.py
@@ -20,16 +20,28 @@ with open(os.path.join(original_file_folder, "imageset_test.txt"), 'r') as f:
 
 root_folder = "data"
 depths = []
+undist_imgs = []
 for chunk in os.listdir(data_folder):
     current_chunk = os.path.join(data_folder, chunk)
+
+    # get depth file list
     depth_folder = os.path.join(current_chunk, "depths")
     current_depths = [os.path.join(root_folder, os.path.join(chunk, os.path.join("depths", filename))) for filename in sorted(os.listdir(depth_folder))]
     depths.extend(current_depths)
 
+    # get the images list
+    undist_imgs_list = os.path.join(current_chunk, "undist_images")
+    current_imgs_list = [os.path.join(root_folder, os.path.join(chunk, os.path.join("undist_images", filename))) for filename in sorted(os.listdir(undist_imgs_list))]
+    undist_imgs.extend(current_imgs_list)
+
+
+# depth_list.txt
 with open(os.path.join(output_folder,"depth_list.txt"), "w") as outfile:
     outfile.write("\n".join(depths))
-# todo depth_list.txt
-# todo image_list.txt
+
+# image_list.txt
+with open(os.path.join(output_folder,"image_list.txt"), "w") as outfile:
+    outfile.write("\n".join(depths))
 # todo imageset_all.txt
 # todo imageset_train.txt
 # todo imageset_test.txt
